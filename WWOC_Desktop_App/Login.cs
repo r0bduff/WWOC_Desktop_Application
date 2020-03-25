0115cbf62e34b3e43719432ea5119d83eaec761b
Robert Duff <rduff154@gmail.com>
2020-03-25 17:15:11 -0500

    Login window fully functional

--- WWOC_Desktop_App/Login.cs
+++ WWOC_Desktop_App/Login.cs
@@ -26,91 +26,226 @@ namespace WWOC_Desktop_App
             using (SqlConnection cnn = new SqlConnection("Data Source=10.135.85.184;Initial Catalog=GROUP4;User ID=Group4;Password=Grp4s2117"))
             {
                 cnn.Open();
-                //string checkUser = "EXEC checkUsername @Username =" + tbUsername.Text + ", @Password =" + tbPassword.Text + ";";
-                string checkUser = "EXEC isUsername @Username =" + tbUsername.Text + ";";
-                SqlCommand sqlComm = new SqlCommand(checkUser, cnn);
-                try
+                string username = tbUsername.Text;
+                string password = tbPassword.Text;
+
+                if(isUsername(username, cnn) == true)
                 {
-                    SqlDataReader reader = sqlComm.ExecuteReader();
-                    if (reader.HasRows == true)//username is in the database
+                    int counter = pullCounter(username, cnn);
+                    if (isLocked(username, counter, cnn) == false)
                     {
-                        user user = new user();
-                        reader.Read();
-                        user.username = reader["username"].ToString();
-                        reader.Close();
-                        //Account Exists time to check the password
-                        string checkPass = "EXEC checkUsername @Username =" + tbUsername.Text + ", @Password =" + tbPassword.Text + ";";
-                        sqlComm = new SqlCommand(checkPass, cnn);
-                        reader = sqlComm.ExecuteReader(); 
-                        if (reader.HasRows == true)
+                        int userId;
+                        if(isPassword(username, password, cnn, out userId) == true)
                         {
-                            reader.Read();
-                            string pullUser = "EXEC PullUser @UserId =" + reader["userID"] + ";";
-                            reader.Close(); //password matches
-                            sqlComm = new SqlCommand(pullUser, cnn);
-                            reader = sqlComm.ExecuteReader(); reader.Read();
-                            //saves info from database to new user object
-                            user.UserID = Convert.ToInt32(reader["userID"]);
-                            user.username = reader["username"].ToString();
-                            user.password = reader["password"].ToString();
-                            user.name = reader["name"].ToString();
-                            user.accessLevel = Convert.ToInt32(reader["accesslevel"]);
-                            user.lockedOut = Convert.ToInt32(reader["lockedOut"]);
-                            user.counter = 0;
-                            reader.Close();
-                            //some security checks
-                            if (user.lockedOut == 1)//checks if user is already locked out
+                            //login successful
+                            user user = getUser(userId, cnn);
+                            pushCounter(user.username, 0, cnn);
+                            Form mainmenu = new MainMenu();
+                            mainmenu.Show();
+                        }
+                        else
+                        {
+                            counter++;
+                            if(isLocked(username, counter, cnn) == true)
                             {
-                                MessageBox.Show("Account is locked");
+                                pushCounter(username, counter, cnn);
+                                MessageBox.Show("Account is locked please contact an admin");
                             }
                             else
                             {
-                                Form MainMenu = new MainMenu();
-                                MainMenu.Show();
-                                this.Hide();
+                                pushCounter(username, counter, cnn);
+                                MessageBox.Show("Login Failed (password)");
                             }
-                            user.updateDatabase();//updates the database after possible adjustments have been made
-                        }//end if username is in the database
-                        else//have to incriment counter for the username
-                        {
-                            string pullCounter = "EXEC pullCounter @Username =" + user.username + ";";
-                            sqlComm = new SqlCommand(pullCounter, cnn);
-                            reader = sqlComm.ExecuteReader(); reader.Read();
-                            user.counter = Convert.ToInt32(reader["counter"]);
-                            user.counter++;
-                            reader.Close();
-                            if(user.counter >= 3)
-                            {
-                                user.lockedOut = 1;
-                                MessageBox.Show("Failed too many attempts account locked");
-                            }
-                            else
-                            {
-                                MessageBox.Show("Invalid Login");
-                            }
-
-                            string pushCounter = "EXEC incrementCounter @Username =" + user.username + ", @Counter =" + user.counter + ";";
-                            sqlComm = new SqlCommand(pushCounter, cnn);
-                            sqlComm.ExecuteNonQuery();
-                        }//end incriment for failed password
-                        
-
-                        
-
-                        
-                    }//end username is not in database
+                        }
+                    }
                     else
                     {
-                        MessageBox.Show("Account does not exist");//tells user username no bueno
+                        MessageBox.Show("Account is locked please contact an admin");
                     }
                 }
-                catch(Exception ex)
+                else
                 {
-                    MessageBox.Show("bad stop it!!! " + ex);
+                    MessageBox.Show("Login Failed (no username)");
                 }
-               
+
+
                 cnn.Close();
+            }//end using
+        }//end button click
+
+        /* Method checks to see if the user is locked out of their account, will update lockedout if found that user is locked out.
+         * Req: string username - username of the user
+         *      int counter - counter of attempts user has made
+         *      SqlConnection cnn - SqlConnection to the database with user information
+         * Returns: Boolean true if the account is locked
+         *          False if the account is not locked
+         */
+        private static Boolean isLocked(string username, int counter, SqlConnection cnn)
+        {
+            try
+            {
+                SqlCommand commLocked = new SqlCommand("EXEC isLocked @Username =" + username + ";", cnn);
+                SqlDataReader reader = commLocked.ExecuteReader(); reader.Read();
+                Boolean test = Convert.ToBoolean(reader["lockedOut"]); reader.Close();
+                reader.Close();
+
+                if(test == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (counter >= 3)
+                    {
+                        commLocked = new SqlCommand("EXEC lockUser @Username =" + username + ";", cnn);
+                        commLocked.ExecuteNonQuery();
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("isLocked broke : " + ex);
+            }
+            return false;
+        }
+
+        /* Method checks to see if the username exists in the database
+         * Req: string username - username of the user
+         *      SqlConnection cnn - SqlConnection to the database with user information
+         * Returns: Boolean true if the account exists
+         *          False if the account does not exist
+         */
+        private static Boolean isUsername(string username, SqlConnection cnn)
+        {
+            try
+            {
+                SqlCommand commUsername = new SqlCommand("EXEC isUsername @Username =" + username + ";", cnn);
+                SqlDataReader reader = commUsername.ExecuteReader(); reader.Read();
+                
+                if (reader.HasRows == true)
+                {
+                    reader.Close();
+                    return true;
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("isUsername broke : " + ex);
+            }
+            return false;
+        }
+
+        /* Method checks to see if the password matches the username
+        * Req: string username - username given by the user
+        *      string password - password given by the user
+        *      SqlConnection cnn - SqlConnection to the database with user information
+        *      int userId - blank int variable to be filled with the userId of the given username
+        * Returns: Boolean true if the password is right
+        *          False if the password is not right
+        *          userId = the matching userID
+        *          userId = 0 if the password didnt match
+        */
+        private static Boolean isPassword(string username, string password, SqlConnection cnn, out int userId)
+        {
+            try
+            {
+                SqlCommand commPassword = new SqlCommand("EXEC checkUsername @Username =" + username + ", @Password =" + password + ";", cnn);
+                SqlDataReader reader = commPassword.ExecuteReader(); reader.Read();
+                if (reader.HasRows == true)
+                {
+                    userId = Convert.ToInt32(reader["userId"]);
+                    reader.Close();
+                    return true;
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("isPassword broke : " + ex);
+            }
+            userId = 0;
+            return false;
+        }
+
+        /* Method that populates a new user object when given a userId
+         * Req: int userId - userId matching a user in the database
+         * Returns: user object containg information from the database matching the given userId
+         */
+        private static user getUser(int userId, SqlConnection cnn)
+        {
+            user user = new user();
+            try
+            {
+                SqlCommand commUser = new SqlCommand("EXEC PullUser @UserId =" + userId + ";", cnn);
+                SqlDataReader reader = commUser.ExecuteReader(); reader.Read();
+
+                user.UserID = userId;
+                user.username = reader["username"].ToString();
+                user.password = reader["password"].ToString();
+                user.name = reader["name"].ToString();
+                user.accessLevel = Convert.ToInt32(reader["accesslevel"]);
+                user.lockedOut = Convert.ToInt32(reader["lockedOut"]);
+                user.counter = 0;
+
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("getUser Broke : " + ex);
+            }
+
+            return user;
+        }
+
+
+        /* Method that pulls the counter for a username
+         * Req: string username - username given by the user
+         *      SqlConnection cnn - SqlConnection to the database with user information
+         * Returns: int containing the current value of the counter
+         */
+        private static int pullCounter(string username, SqlConnection cnn)
+        {
+            int counter = -1;
+            try
+            {
+                string pullCounter = "EXEC pullCounter @Username =" + username + ";";
+                SqlCommand commCounter = new SqlCommand(pullCounter, cnn);
+                SqlDataReader reader = commCounter.ExecuteReader(); reader.Read();
+                counter = Convert.ToInt32(reader["counter"]);
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("pullCounter Broke : " + ex);
+            }
+            return counter;
+        }
+
+
+        /* Methos that updates the counter for a given username.
+         * Req: string username - username given by the user
+         *      int counter - current value of the counter
+         *      SqlConnection cnn - SqlConnection to the database with user information
+         * Returns: nothing
+         */
+        private void pushCounter(string username, int counter, SqlConnection cnn)
+        {
+            try
+            {
+                string pushCounter = "EXEC incrementCounter @Username =" + username + ", @Counter =" + counter + ";";
+                SqlCommand commCounter2 = new SqlCommand(pushCounter, cnn);
+                commCounter2.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("incemenetCounter Broke : " + ex);
             }
         }
-    }
+
+
+        
+    }//end login class
 }
